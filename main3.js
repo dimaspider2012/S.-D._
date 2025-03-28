9f1dacf085496227d038b4e963ba3035228519f0
Dima <dimaspider2012@gmail.com>
2025-03-28 18:30:47 +0200

    commit 3

--- main3.js
+++ main3.js
@@ -6,26 +6,35 @@ const message = document.querySelector(".message")
 buttonlog.addEventListener("click", () => {
   if (login.value === "" || password.value === "") {
     console.log("Будь ласка, заповніть всі поля.");
-    return;
+    message.textContent = "Error, we cannot find this login"
+    message.style.color = "red"
   }
   
   if (login.value === "Admin" && password.value === "123") {
+    console.log("Login found");
+    message.textContent = "Login found";
+    message.style.color = "green";
     window.location.href = "4.html";
-  } else {
+  } else if (!login.value === "Admin" && password.value === "123") {
     console.log("Error, we cannot find this login");
-    message.style.textContent = "Error, we cannot find this login"
-    message.style.color = "red"
   }
 });
 
 buttonlogB.addEventListener("click", () => {
   if (login.value === "" || password.value === "") {
     console.log("Будь ласка, заповніть всі поля.");
+     message.textContent = "Error, we cannot find this login"
+    message.style.color = "red"
   }
   
   if (login.value === "Admin" && password.value === "123") {
+    console.log("Login found");
+    message.textContent = "Login found";
+    message.style.color = "green";
     window.location.href = "5.html";
   } else {
-    console.log("Error, we cannot find this login");
+     console.log("Error, we cannot find this login");
+     message.textContent = "Error, we cannot find this login"
+     message.style.color = "red"
   }
 });
